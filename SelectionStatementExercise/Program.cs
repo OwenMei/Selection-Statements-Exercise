ea43d5559043b8984ecbff7ccdd0c6ebbcc28988
owen mei <owenzackmei@gmail.com>
2023-06-21 11:23:42 -0400

    init commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random randomNum = new Random();
+            int favNum = randomNum.Next(1,100);
+            int guess = -1;
+            Console.WriteLine("guess a number");
+            while (favNum != guess)
+            {
+                guess = (int.TryParse(Console.ReadLine(), out int guessed)) ? guessed : 0;
+                if(favNum < guess)
+                {
+                    Console.WriteLine("too high");
+                }
+                else if(guess < favNum)
+                {
+                    Console.WriteLine("too low");
+                }
+                else
+                {
+                    Console.WriteLine($"Yeah! The number was {favNum}");
+                }
+                
+            }
+
         }
     }
 }
